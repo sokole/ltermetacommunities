cc254943fc4961e630ee4ed13d5334c0fbb5d9a9
sokole <sokole@gmail.com>
2016-03-11 11:31:46 -0700

    sync sokol dev script

--- RSCRIPTS_diversity_calcs/RCODE_1_9000_diversity_metrics.R
+++ RSCRIPTS_diversity_calcs/RCODE_1_9000_diversity_metrics.R
@@ -18,28 +18,35 @@ d.xy <- read.csv('DATA_IN/dat_simulated_site_coords.csv')
 
 # -- make vegetarian::d compatible with long data, send it two vectors
 fn.divpart.long <- function(
+  site.id.vect,
   spp.vect,
   abund.vect,
   ...){
-  df.long <- data.frame(spp.vect, abund.vect)
-  df.wide <- tidyr::spread(df.long, spp.vect, abund.vect,
-                           fill = 0)
+  df.wide <- tidyr::spread(
+    data.frame(
+      site.id.vect,
+      spp.vect,
+      abund.vect
+    ),
+    spp.vect,
+    abund.vect,
+    fill = 0)[,-1]
+  
   vegetarian::d(df.wide, wts = rowSums(df.wide), ...)
 }
 
+# fn.divpart.long(d.1.long$spp, d.1.long$count, lev='gamma', q=1)
 # ---------------------------------------------------------------
 
 # -- group by time
 
 dat_diversities_by_timestep <- d.comm.long %>% group_by(timestep) %>% 
   summarise(
-    alpha_q1 = fn.divpart.long(spp, count, lev = 'alpha', q = 1),
-    beta_q1 = fn.divpart.long(spp, count, lev = 'beta', q = 1),
-    gamma_q1 = fn.divpart.long(spp, count, lev = 'gamma', q = 1),
-    alpha_q2 = fn.divpart.long(spp, count, lev = 'alpha', q = 2),
-    beta_q2 = fn.divpart.long(spp, count, lev = 'beta', q = 2),
-    gamma_q2 = fn.divpart.long(spp, count, lev = 'gamma', q = 2)
+    alpha_q1 = fn.divpart.long(site, spp, count, lev = 'alpha', q = 1),
+    beta_q1 = fn.divpart.long(site, spp, count, lev = 'beta', q = 1),
+    gamma_q1 = fn.divpart.long(site, spp, count, lev = 'gamma', q = 1),
+    alpha_q2 = fn.divpart.long(site, spp, count, lev = 'alpha', q = 2),
+    beta_q2 = fn.divpart.long(site, spp, count, lev = 'beta', q = 2),
+    gamma_q2 = fn.divpart.long(site, spp, count, lev = 'gamma', q = 2)
   )
-
-write.csv(dat_diversities_by_timestep, 'data_out_diversities_by_timestep.csv', row.names = FALSE)
 
