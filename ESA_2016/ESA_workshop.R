1626941b7a2b024c1f0837a4620b60aa4ae54bc9
Nathan Wisnoski <wisnoski@indiana.edu>
2016-07-13 14:56:45 -0400

    fixed NA issue

--- ESA_2016/ESA_workshop.R
+++ ESA_2016/ESA_workshop.R
@@ -14,21 +14,17 @@ for (package in c('dplyr', 'tidyr', 'vegetarian', 'vegan', 'metacom')) {
   }
 }
 
-# Load packages
-library(dplyr)
-library(tidyr)
-library(vegetarian)
-library(vegan)
-library(metacom)
-
 # Read in NWT plant community data and site coordinates 
 nwt.xy <- read.csv("ESA_2016/NWT_coordinates.csv")
 nwt.comm.long <- read.csv("ESA_2016/NWT_plantcomp.csv")[,c(2,4,5,6)]
-dim(nwt.comm.long)
+nwt.comm.wide <- tidyr::spread(nwt.comm.long, 
+                               USDA_code, abund,
+                               fill = 0)
 
-nwt.comm.wide <- tidyr::spread(nwt.comm.long, USDA_code, abund)
+nwt.comm.wide
 
 group_by(nwt.comm.wide, year)
+
 
 # Function to pass long form data frames to metacom package
 fn.ems.long <- function(
@@ -45,15 +41,27 @@ fn.ems.long <- function(
   spp.vect,
   abund.vect,
   fill = 0)[,-1]
+  
+  # df.wide <- df.wide[,-which(colSums(df.wide) == 0)]
+  # df.wide <- df.wide[-which(rowSums(df.wide) == 0),]
 
   IdentifyStructure(metacom::Metacommunity(
-    comm = vegan::decostand(df.wide, method = 'pa'),
-    method = 'r1', sims = 10))
+    comm = (df.wide > 0) * 1,...))
 }
 
-#### We need to figure out NA handling, lots of NAs in the NWT dataset
+
 
 # Create data frame of EMS for each time step
-ems.by.timestep <- nwt.comm.long %>% group_by(year) %>% summarise(
-  ems = fn.ems.long(plot, USDA_code, abund)
+ems.by.year <- nwt.comm.long %>% group_by(year) %>% summarise(
+  ems = fn.ems.long(plot, USDA_code, abund, method = 'r1', sims = 10)
 )
+
+# Test on individual year
+nwt.1989 <- filter(nwt.comm.wide, year == 1989)
+head(nwt.1989)
+nwt.1989 <- nwt.1989[,-which(colSums(nwt.1989) == 0)]
+colSums(nwt.1989) == 0
+ems.1989 <- Metacommunity(
+  decostand(nwt.1989[,-c(1:2)], method = "pa"),
+  method = "r1", sims = 10)
+IdentifyStructure(ems.1989)
