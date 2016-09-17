d3599518c7ed016775e2f6b7f1d4cf2051b85e7c
Nathan Wisnoski <wisnoski@indiana.edu>
2016-09-17 14:48:31 -0700

    Pulled out the spatial matrix from the long format data

--- Group1-diversity-metrics/update-code-for-sbc-dataset.R
+++ Group1-diversity-metrics/update-code-for-sbc-dataset.R
@@ -4,5 +4,13 @@
 # http://www.labnol.org/internet/direct-links-for-google-drive/28356/
 # Get id number from sharing link and use to create download link
 
+library(tidyr)
+library(dplyr)
+
 download.link <- "https://drive.google.com/uc?export=download&id=0BxUZSA1Gn1HZYTVfd2FZTWhWbm8"
 sbc.data <- read.csv(file = download.link, header = T)
+
+# Create the spatial dataset
+sbc.spatial.long <- sbc.data[which(sbc.data$OBSERVATION_TYPE == "SPATIAL_COORDINATE"),
+                        which(colnames(sbc.data) %in% c("SITE_ID", "VARIABLE_NAME", "VALUE"))]
+sbc.spatial.wide <- tidyr::spread(sbc.spatial, key = VARIABLE_NAME, value = VALUE)
