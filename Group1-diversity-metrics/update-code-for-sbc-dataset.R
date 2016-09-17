bffc0dadfc80fdf689f1eca7eec0e679931eb027
Nathan Wisnoski <wisnoski@indiana.edu>
2016-09-17 14:54:29 -0700

    Pulled out the environmental matrix

--- Group1-diversity-metrics/update-code-for-sbc-dataset.R
+++ Group1-diversity-metrics/update-code-for-sbc-dataset.R
@@ -13,4 +13,13 @@ sbc.data <- read.csv(file = download.link, header = T)
 # Create the spatial dataset
 sbc.spatial.long <- sbc.data[which(sbc.data$OBSERVATION_TYPE == "SPATIAL_COORDINATE"),
                         which(colnames(sbc.data) %in% c("SITE_ID", "VARIABLE_NAME", "VALUE"))]
-sbc.spatial.wide <- tidyr::spread(sbc.spatial, key = VARIABLE_NAME, value = VALUE)
+sbc.spatial.wide <- tidyr::spread(sbc.spatial.long, key = VARIABLE_NAME, value = VALUE)
+
+# Create the environmental dataset
+sbc.env.long <- sbc.data[which(sbc.data$OBSERVATION_TYPE == "ENV_VAR"),
+                             which(colnames(sbc.data) %in% 
+                                     c("SITE_ID", "DATE",
+                                       "VARIABLE_NAME", "VALUE"))]
+
+sbc.env.wide <- tidyr::spread(sbc.env.long, key = VARIABLE_NAME, value = VALUE)
+
