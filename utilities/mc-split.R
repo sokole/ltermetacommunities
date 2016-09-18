dc2f26d9326d1135c4b5be1b7dc3822a5eeb84d5
Nathan Wisnoski <wisnoski@indiana.edu>
2016-09-18 17:58:22 -0400

    added function to split long-form dataset into long and wide form species, env, and spatial datasets

---
+++ utilities/mc-split.R
@@ -0,0 +1,21 @@
+fn.mc.split <- function(mc.data = mc.data){
+  
+  # Create the spatial dataset
+  mc.spatial.long <<- filter(mc.data, OBSERVATION_TYPE == "SPATIAL_COORDINATE") %>% 
+    select(SITE_ID, VARIABLE_NAME, VALUE)
+  
+  mc.spatial.wide <<- arrange(
+    tidyr::spread(mc.spatial.long, key = VARIABLE_NAME, value = VALUE), SITE_ID)
+  
+  # Create the environmental dataset
+  mc.env.long <<- filter(mc.data, OBSERVATION_TYPE == "ENV_VAR") %>% 
+    select(SITE_ID, DATE, VARIABLE_NAME, VALUE)
+  
+  mc.env.wide <<- arrange(
+    tidyr::spread(mc.env.long, key = VARIABLE_NAME, value = VALUE), SITE_ID)
+  
+  # Create the species matrix
+  mc.species.long <<- filter(na.omit(mc.data), OBSERVATION_TYPE == "TAXON_COUNT") %>%
+    select(SITE_ID, DATE, VARIABLE_NAME, VALUE)
+  mc.species.wide <<- tidyr::spread(mc.species.long, key = VARIABLE_NAME, value = VALUE, fill = 0)
+}
