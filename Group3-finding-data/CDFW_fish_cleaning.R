47329e0897b1d29b48d35603342eb48a5239643a
Lauren Hallett <lauren.hallett@berkeley.edu>
2016-09-18 17:18:32 -0700

    adds file to clean cdfw data

--- Group3-finding-data/CDFW_fish_cleaning.R
+++ Group3-finding-data/CDFW_fish_cleaning.R
@@ -1,9 +1,18 @@
+## Code to clean the California Fish and Wildlife's Fish Survey Data
+## NOTE: Full metadata is in a separate .csv on google drive
+## NOTE: Current version includes jelly fish - remove?
+## NOTE: Current version does not have lat/long - need to ask for it? 
+## The website has an interactive map with the sites, so coordinates are definitely known...
+
 library(tidyr)
 library(dplyr)
 
 rm(list = ls())
 
+# key to the website from google drive
 fishkey <- "0BwguSiR80XFZM3ZranBtczRrbjA" # Google Drive file ID
+
+# clean the spp data
 cdfw.spp <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", fishkey)) %>%
   tbl_df() %>%
   mutate(Stripped.Bass = Striped.Bass.age.0 + Striped.Bass.age.1 + Striped.Bass.age.2 + Striped.Bass.age.3.) %>%
@@ -18,6 +27,7 @@ cdfw.spp <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download",
          VALUE = ifelse(is.na(VALUE), 0, VALUE)) %>%
   select(OBSERVATION_TYPE, SITE_ID, DATE, VARIABLE_NAME, VARIABLE_UNITS, VALUE)
   
+# clean the environmental data
 cdfw.env <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", fishkey)) %>%
   tbl_df() %>%
   mutate(MaxTemp = Top_TempC, 
