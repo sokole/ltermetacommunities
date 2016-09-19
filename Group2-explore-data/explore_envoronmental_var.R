1a573f3cf4d96f823ecfa1dc3a0b95add9105e9d
ninalany <lanynina@msu.edu>
2016-09-19 14:24:49 -0700

    make code compatible with other scripts

--- Group2-explore-data/explore_envoronmental_var.R
+++ Group2-explore-data/explore_envoronmental_var.R
@@ -1,12 +1,29 @@
-# ------------------------------------- #
-# Reformat SBC data - 17 September 2016 #
-# ------------------------------------- #
+# ----------------------------------------------- #
+#  Explore environmental data - 19 September 2016 #
+# ----------------------------------------------- #
 
 # Set working environment
 rm(list = ls())
-setwd("~/Google Drive/LTER-DATA/SBC-Lamy-Castorani/")
 
-# Check for and install required packages
+# Assign data set of interest
+
+# CAP LTER (Central Arizona-Phoenix)
+#data.set <- "CAP-birds-CORE"
+#data.key <- "0BzcCZxciOlWgeHJ5SWx1YmplMkE" # Google Drive file ID (different for each dataset)
+
+# NWT LTER (Niwot Ridge)
+#data.set <- "NWT-plants-Hallett-and-Sokol"
+#data.key <- "0B2P104M94skvQVprSnBsYjRzVms" # Google Drive file ID (different for each dataset)
+
+# SBC LTER (Santa Barbara Coastal)
+data.set <- "SBC-Lamy-Castorani"
+data.key <- "0BxUZSA1Gn1HZYTVfd2FZTWhWbm8" # Google Drive file ID (different for each dataset)
+
+# 
+
+# Set working environment
+setwd(paste("~/Google Drive/LTER-DATA/", data.set, sep=""))
+
 library()
 
 for (package in c('dplyr', 'tidyr', 'vegetarian', 'vegan', 'metacom', 'ggplot2')) {
@@ -16,43 +33,37 @@ for (package in c('dplyr', 'tidyr', 'vegetarian', 'vegan', 'metacom', 'ggplot2')
   }
 }
 
-
-id.long.dat <- "0BxUZSA1Gn1HZYTVfd2FZTWhWbm8"
-sbc.long <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id.long.dat)) 
-
-
-dat.long <- sbc.long
-
-# Check types of taxa
-unique(dat.long$TAXON_GROUP)
+# IMPORT DATA
+dat.long <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", data.key)) 
 
 str(dat.long)
 
 #make dataframe of site-year data
 #unique(dat.long$OBSERVATION_TYPE)
-site_yr <- subset(dat.long, OBSERVATION_TYPE == "ENV_VAR")
-git add G	
-str(site_yr)
-site_yr <- droplevels(site_yr)
+env.long <- subset(dat.long, OBSERVATION_TYPE == "ENV_VAR")
+env.long <- droplevels(env.long)
+str(env.long)
+
+levels(env.long$VARIABLE_UNITS)
+levels(env.long$VARIABLE_NAME)
 
 
-levels(site_yr$VARIABLE_UNITS)
-levels(site_yr$VARIABLE_NAME)
+#Plot time series of environmental variabLes for ech site
 
-
-#Plot time series of environmental varialbes for ech site
-
-env.plot <- ggplot(data = site_yr, aes(x = DATE, y = VALUE)) +
+env.plot <- ggplot(data = env.long, aes(x = DATE, y = VALUE)) +
 geom_line(aes(color = SITE_ID)) +
 facet_wrap(~ VARIABLE_NAME, scales = "free", ncol = 1) +
 theme_bw()
 env.plot
-? facet_wrap
-plot(y = site_yr[site_yr$VARIABLE_NAME == "WAVE_HT_MEAN", ]$VALUE, x = site_yr[site_yr$VARIABLE_NAME == "WAVE_HT_WINTER_MEAN", ]$VALUE)
+
+plot(y = env.long[env.long$VARIABLE_NAME == "WAVE_HT_MEAN", ]$VALUE, x = env.long[env.long$VARIABLE_NAME == "WAVE_HT_WINTER_MEAN", ]$VALUE)
 
 #Look at pairwise correlation between envoronmental predictors
-##NOTE Data must be win wide form
-id.env <- "0BxUZSA1Gn1HZZDF5cGpnLWtnWGc" # Google Drive file ID
-site_yr_wide <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id.env))
-str(site_yr_wide)
-pairs(site_yr_wide[,c("TEMP_MEAN_C", "WAVE_HT_MEAN","WAVE_HT_WINTER_MEAN")])
+##NOTE Data must be in wide form
+  env.wide <- env.long %>%
+  select(-VARIABLE_UNITS) %>%
+    tidyr::spread(VARIABLE_NAME,  VALUE)
+
+
+str(env.wide)
+pairs(env.wide[,c("TEMP_MEAN_C", "WAVE_HT_MEAN","WAVE_HT_WINTER_MEAN")])
