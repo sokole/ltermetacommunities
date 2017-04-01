c270c8b9d65c455cca4e78982618951780f10b2b
ThomasLamy <thomas.lamy27@gmail.com>
2017-03-31 20:27:40 -0700

    # editing function aggregate stability

--- Group3-diversity-metrics/Aggregate_stability.R
+++ Group3-diversity-metrics/Aggregate_stability.R
@@ -24,7 +24,7 @@ aggregate_stab <- function(Y, s, t)
 # edited September 28th 2016
 {
 
-# compute total community biomass
+# compute total community biomass (i.e. the aggregate community metric)
 tot.bio <- apply(Y, 1, sum)
 
 # check that data is balanced
@@ -33,33 +33,54 @@ if(length(tot.bio) != s * t) cat("STOP: sites are not surveyed every years")
 # matrix of Ni(t) with sites as row and time as colums
 Nit <- matrix(tot.bio, nrow=s, ncol=t, byrow=TRUE)
 
-# compute the average temporal biomass at each site
-mui <- apply(Nit, 1, mean)
-
-# compute the variance-covariance matrix of temporal biomass across sites [W = wij of N(t)]
+# variance-covariance matrix of the temporal biomass across sites [W = wij of N(t)]
 W <- cov(t(Nit))
 
-# get the temporal variance (sd) of the communities in each of the m sites
+#### local scale aggregate variability ####
+# compute the average total biomass at each site
+mui <- apply(Nit, 1, mean)
+
+# get the variance, and sd, of the total biomass in each of the s sites
 Vari <- diag(W)
 sigi <- sqrt(diag(W))
-# get the sum of the temporal covariances of the communities in each of the m sites
-Covsum <- sum(W[lower.tri(W, diag=FALSE)])
 
-#### local scale temporal variability ####
 # temporal variability of the ith community is calculated as the coefficient of temporal variation of its biomass
 CVi <- sigi/mui
+
 # compute the temporal mean of the total metacommunity biomass
 muM <- sum(mui)
+
 # temporal variability at the local scale is defined as the weighted average of CVi across local sites
 CVL <- sum(sigi)/muM  # = sum(mui/muM*CVi)
-# local variability is the squared coefficient of temporal variations at local scale
+# local aggregate variability is the squared coefficient of temporal variations at local scale
 CVL2 <- CVL^2
 
 #### Metacommunity/regional scale temporal variability ####
+# get the sum of the temporal covariances of the communities in each of the m sites
+Covsum <- sum(W)
 # temporal variability at the metacommunity scale is the coefficient of temporal variation of metacommunity biomass
 CVM <- sqrt(Covsum)/muM
-# Metacommunity variability is the squared coefficient of temporal variations at metacommunity scale
+# Metacommunity aggregate variability is the squared coefficient of temporal variations at metacommunity scale
 CVM2 <- CVM^2
+
+## Total biomass at the metacommunity level
+NiM <- apply(Nit, 2, sum)
+
+# Metacommunity aggregate variability can also be compute based on:
+# metacommunity temporal variance
+# var(NiM) # = Covsum <- sum(W)
+# temporal average of the total metacommunity biomass
+# mean(NiM) # = muM
+## whereby regional CV
+# sqrt(var(NiM))/mean(NiM)
+# CVM2 = var(NiM)/mean(NiM)^2
+
+# plot total community biomass across sites and over time
+if(plot){ 
+  plot(1:t, NiM, type="l", ylab="Total community biomass", xlab="Time", main="", lwd = 4, ylim = c(0, max(NiM)))
+  coli <- rainbow(s)
+  for (i in 1:s) lines(Nit[i,], lwd = 2, lty = 2, col = coli[i])	
+}
 
 #### Spatial variability at the metacommunity scale ####
 # compute the average biomass of local communities at each time t
@@ -69,7 +90,7 @@ VSt <- c(); for (i in 1:t) VSt[i] <- sum((Nit[,i] - Nbart[i])^2)/s
 
 # mubar is the spatial average of the temporal mean of local community biomass
 mubar <- mean(mui)
-## spatial average of the standard deviation of local community biomass
+# spatial average of the standard deviation of local community biomass
 sqrtwbar <- mean(sigi) 
 # We now compute spatial variability as
 CVS2 <- 1/(mubar^2)*(sqrtwbar^2 - ((1/s^2)*Covsum) + (1/s)*sum((mui - mubar)^2 + (sigi - sqrtwbar)^2))
@@ -92,7 +113,7 @@ phi <- Covsum/((sum(sigi))^2)
 Beta1 <- 1/phi ##  spatial asynchrony-related variability
 
 # summary result
-res <- data.frame(variable = c("Gamma", "Alpha", "Beta", "Phi"), val=c(GammaCV, AlphaCV, Beta1, phi))
+res <- data.frame(GammaCV = GammaCV, AlphaCV = AlphaCV, PhiCV = phi)
 
 res
 }
