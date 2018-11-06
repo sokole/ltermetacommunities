153033e9a9a830fde4106cfe1725c9f6aa5f8c6a
Compagnoni <ac22qawo@idiv.de>
2018-11-06 15:33:33 -0700

    Format according to ltermetacommunities

--- Group2-explore-data/format_data/CDR-grasshoppers_format_data.R
+++ Group2-explore-data/format_data/CDR-grasshoppers_format_data.R
@@ -38,22 +38,32 @@ cdr     <- cdr_raw %>%
                     species = replace(species, genus == 'Scudderia', 'spp.'),
                     species = replace(species, genus == 'Tetrix', 'spp.') ) %>% 
             # remove genus-level IDs for Melanopus
-            subset( !(genus == 'Melanoplus' & species == 'spp.') )
-
-# look up the taxonomy 
-taxa <- cdr_raw %>% 
-          group_by( Year, Month, Field.num, Order, Family, Genus, Specific.epithet) %>% 
-          summarise( count = sum(X.Specimens, na.rm=T) ) %>% 
-          ungroup %>% 
-          rename( genus   = Genus,
-                  species = Specific.epithet) %>% 
-          mutate( species = replace(species, species == 'undet', 'spp.')) %>% 
-          group_by(genus, species) %>%
-          summarise(total = sum(count) ) %>% 
-          as.data.frame %>% 
-          arrange(genus, species)
-          
+            subset( !(genus == 'Melanoplus' & species == 'spp.') ) %>% 
+            select( -Order,-Family ) %>% 
+  
+            # create ltermetacomm format
+            mutate( DATE             = paste(Year, Month, sep = '_'),
+                    OBSERVATION_TYPE = 'TAXON_COUNT',
+                    VARIABLE_NAME    = paste(genus, species, sep = '_'), 
+                    VARIABLE_UNITS   = 'COUNT (monthly sum)' ) %>% 
+            rename( VALUE            = count ) %>% 
+            select(OBSERVATION_TYPE,SITE_ID, DATE, VARIABLE_NAME, VARIABLE_UNITS, VALUE)
  
+  
+# Total sums for taxa, to check % of taxa info at the genus level
+taxa <- cdr_raw %>% 
+            group_by( Year, Month, Field.num, Order, Family, Genus, Specific.epithet) %>% 
+            summarise( count = sum(X.Specimens, na.rm=T) ) %>% 
+            ungroup %>% 
+            rename( genus   = Genus,
+                    species = Specific.epithet) %>% 
+            mutate( species = replace(species, species == 'undet', 'spp.')) %>% 
+            group_by(genus, species) %>%
+            summarise(total = sum(count) ) %>% 
+            as.data.frame %>% 
+            arrange(genus, species)
+          
+
 # write file out
 write.csv(cdr, '~/Google Drive File Stream/My Drive/LTER Metacommunities/LTER-DATA/L3-aggregated_by_year_and_space/L3-cdr-grasshopper-compagnoni.csv', row.names=F)
 
