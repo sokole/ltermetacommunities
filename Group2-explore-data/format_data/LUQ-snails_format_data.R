b981a71cf4573b135718943c0ddc13d015750f28
Compagnoni <ac22qawo@idiv.de>
2018-11-07 16:35:22 -0700

    Format LUQ snail data according to ltermetacommunities data standard

--- Group2-explore-data/format_data/LUQ-snails_format_data.R
+++ Group2-explore-data/format_data/LUQ-snails_format_data.R
@@ -50,7 +50,7 @@ snail     <-read.csv(infile1,
                         "Total.snail.abundance",     
                         "Comments.at.the.field"    ), check.names=TRUE)
 
-
+# check how many sites we have every single year
 snail %>% 
   select(Year, Point ) %>% 
   unique %>% 
@@ -75,60 +75,19 @@ snail_long <- snail %>%
                 separate( Date, c('month','day','year'), sep='/' ) %>% 
                 # get count averages
                 group_by( year, spp_code, Point) %>% 
-                summarise( count = mean(count, na.rm=T) )
+                summarise( count = mean(count, na.rm=T) ) %>% 
 
-# read data directly from the portal (no idea why this is not in popler.....)
-cdr_raw <- read.csv('https://portal.lternet.edu/nis/dataviewer?packageid=knb-lter-cdr.106.8&entityid=3405c2e271929b0c537492a9ddde102b',
-                sep = '\t') 
+                # LTERMETACOMM FORMAT
+                rename( SITE_ID       = Point,
+                        VARIABLE_NAME = spp_code,
+                        DATE          = year,
+                        VALYE         = count ) %>% 
+                mutate( OBSERVATION_TYPE = 'TAXON_COUNT',
+                        VARIABLE_UNITS   = 'COUNT (yearly mean)' ) %>% 
+                # order data
+                select(OBSERVATION_TYPE, SITE_ID, DATE, 
+                       VARIABLE_NAME, VARIABLE_UNITS, VALUE)
 
-# clean up dataset
-cdr     <- cdr_raw %>% 
-            # create site information
-            mutate( SITE_ID = as.character(Field.num),
-                    yr_mon  = paste(Year,Month,sep='_') ) %>% 
-            # select only sites with continuous data
-            subset( !(SITE_ID == '28' | SITE_ID == '11') ) %>% 
-            # aggregate by species/month
-            group_by( Year, SITE_ID, Order, Family, Genus, Specific.epithet) %>% 
-            # sum across all months in a year. 
-            # Sampling mostly consistent, excet for 2003, when June and August samples were lost in SOME fields
-            summarise( count = sum(X.Specimens, na.rm=T) ) %>% 
-            ungroup %>% 
-            rename( genus   = Genus,
-                    species = Specific.epithet ) %>% 
-            mutate( species = replace(species, species == 'undet', 'spp.')) %>% 
-  
-            # Fix taxonomic information
-            # species to "Lump" to genus (too high proportion of IDs at the genus level only)
-            mutate( species = replace(species, genus == 'Conocephalus', 'spp.'),
-                    species = replace(species, genus == 'Scudderia', 'spp.'),
-                    species = replace(species, genus == 'Tetrix', 'spp.') ) %>% 
-            # remove genus-level IDs for Melanopus
-            subset( !(genus == 'Melanoplus' & species == 'spp.') ) %>% 
-            select( -Order,-Family ) %>% 
-  
-            # create ltermetacomm format
-            mutate( OBSERVATION_TYPE = 'TAXON_COUNT',
-                    VARIABLE_NAME    = paste(genus, species, sep = '_'), 
-                    VARIABLE_UNITS   = 'COUNT (monthly sum)' ) %>% 
-            rename( DATE             = Year, 
-                    VALUE            = count ) %>% 
-            select(OBSERVATION_TYPE,SITE_ID, DATE, VARIABLE_NAME, VARIABLE_UNITS, VALUE)
- 
-  
-# Total sums for taxa, to check % of taxa info at the genus level
-taxa <- cdr_raw %>% 
-            group_by( Year, Month, Field.num, Order, Family, Genus, Specific.epithet) %>% 
-            summarise( count = sum(X.Specimens, na.rm=T) ) %>% 
-            ungroup %>% 
-            rename( genus   = Genus,
-                    species = Specific.epithet) %>% 
-            mutate( species = replace(species, species == 'undet', 'spp.')) %>% 
-            group_by(genus, species) %>%
-            summarise(total = sum(count) ) %>% 
-            as.data.frame %>% 
-            arrange(genus, species)
-          
 # write file out
-write.csv(cdr, '~/Google Drive File Stream/My Drive/LTER Metacommunities/LTER-DATA/L3-aggregated_by_year_and_space/L3-cdr-grasshopper-compagnoni.csv', row.names=F)
+write.csv(snail_long, 'C:/L3-luq-snails-compagnoni.csv', row.names=F)
 
