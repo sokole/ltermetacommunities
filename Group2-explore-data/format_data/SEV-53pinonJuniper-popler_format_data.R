4b354d7dad596e09bd84b246fb6d7ad13c70b7c6
Compagnoni <ac22qawo@idiv.de>
2018-12-19 11:44:32 +0100

    Remove winter season, plot 3, assign season to its corresponding month

--- Group2-explore-data/format_data/SEV-53pinonJuniper-popler_format_data.R
+++ Group2-explore-data/format_data/SEV-53pinonJuniper-popler_format_data.R
@@ -79,45 +79,51 @@ google_id <- ecocom_dp_dir %>% filter(grepl('taxon',name)) %>% select(id) %>% un
 dt5 <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", google_id))
 
 # read directly from EDI internet site
-infile1   <- "https://pasta.lternet.edu/package/data/eml/knb-lter-sev/278/245672/43eb46261b04ab1e5d250b86ebf7fc35" 
-infile1   <- sub("^https","http",infile1) 
-dt1       <-read.csv(infile1,header=F,skip=1,sep=","  
-        , col.names=c(
-                    "year",     
-                    "season",     
-                    "date",     
-                    "site",     
-                    "treatment",     
-                    "plot",     
-                    "transect",     
-                    "quad",     
-                    "species",     
-                    "obs",     
-                    "cover",     
-                    "height",     
-                    "count",     
-                    "comments"    ), 
-                    check.names=TRUE,
-                    stringsAsFactors = F)  
-  
+infile1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-sev/278/245672/43eb46261b04ab1e5d250b86ebf7fc35" 
+infile1 <- sub("^https","http",infile1) 
+dt1     <-read.csv(infile1,header=F,skip=1,sep=",", 
+                   col.names=c( "year",     
+                                "season",     
+                                "date",     
+                                "site", 
+                                "plot",     
+                                "transect",     
+                                "quad",
+                                "treatment",     
+                                "species",     
+                                "obs",     
+                                "cover",     
+                                "height",     
+                                "count",     
+                                "comments"), 
+                                check.names=TRUE,
+                                stringsAsFactors = F)
+
 # function to remove NAs (-888)
-remove_888 <- function(x) replace(x, x == -888, NA)
 remove_999 <- function(x) replace(x, x == -999, NA)
       
-
+# format sev plants
 sev <- dt1 %>% 
-          # remove season 1 (not always sampled)
+          # remove season 1 (winter, not always sampled)
           subset( !(season %in% 1) ) %>% 
           # remove lines with -999
           subset( !(count %in% -999) ) %>% 
           subset( !(species %in% -999) ) %>% 
+          # remove plots connected to different transects (A, B, C, D)
+          subset( !(plot %in% 3) ) %>% 
+          # assign the month of year connected to "season"
+          mutate( season = replace(season, 
+                                   season == 2,
+                                   5) ) %>% 
+          mutate( season = replace(season, 
+                                   season == 3,
+                                   9) ) %>% 
           mutate( DATE = paste(year,season,sep='.') ) %>% 
           mutate( DATE = as.numeric(DATE) ) %>% 
-          group_by( DATE, site, plot, species) %>% 
+          group_by( DATE, site, plot, transect, species) %>% 
           summarise( VALUE = sum(count,na.rm=T) ) %>% 
           ungroup() %>% 
-          mutate( SITE_ID = paste(site,
-                                  plot,sep='_') ) %>% 
+          mutate( SITE_ID = paste(site,plot,transect, sep='_') ) %>% 
           rename( VARIABLE_NAME   = species ) %>% 
           mutate( VARIABLE_UNITS  = 'count') %>% 
           mutate(OBSERVATION_TYPE = 'TAXON_COUNT') %>%
@@ -129,17 +135,8 @@ sev <- dt1 %>%
           gather(VARIABLE_NAME, VALUE, -DATE, -SITE_ID, -OBSERVATION_TYPE, -VARIABLE_UNITS) %>% 
           unique
 
-L3dat<-sev
-# check replication
-ggplot(data  = sev, 
-       aes(x = DATE,
-           y = SITE_ID)) +
-    geom_point(size = 5) +
-    theme_bw() + 
-    xlab("Year with available data") + 
-    ylab("Site")
-
- 
+# write it out
+write.csv(sev, 'L3-sev-plants-compagnoni.csv', row.names=F)
  
 #2 Make TAXON_COUNT from the Observation table (dt1)
 #select the columns of interest
